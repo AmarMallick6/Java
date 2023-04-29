d80baa9d4adfa8bb2e90a8351aadb0a89ba384ef
MAK <MAK@143MAK>
2023-04-29 12:49:03 +0530

    2nd Commit

--- Java/src/datatype/Premitive.java
+++ Java/src/datatype/Premitive.java
@@ -3,8 +3,20 @@ package datatype;
 public class Premitive {
 
 	public static void main(String[] args) {
+		
 	byte no=5;
 	System.out.println("Number is "+ no);
+	
+	short age=55;
+	System.out.println("Age is "+ age);
+	
+	int salary=50000;
+	System.out.println("Salary is "+ salary);
+	
+	long turnover=500000;
+	System.out.println("Company Turnover is "+turnover);
+	
+	
 	}
 
 }
